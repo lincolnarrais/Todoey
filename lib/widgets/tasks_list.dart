0eaad38f897096f71408ecbd3093b901de10b5c6
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-07-26 13:48:03 -0300

    App final no curso (com alguns tweaks que fiz ao longo do caminho, principalmente no final).

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -17,7 +20,61 @@ class TasksList extends StatelessWidget {
               taskName: task.name,
               isChecked: task.isDone,
               toggleCheckboxState: () => taskData.toggleCheckboxState(task),
-              onLongPress: () => taskData.deleteTask(task),
+              onLongPress: () {
+                Platform.isIOS
+                    ? showCupertinoDialog(
+                        context: context,
+                        builder: (_) {
+                          return CupertinoAlertDialog(
+                            title: Text('DELETE TASK'),
+                            content: Text('Do you wish to delete this task?'),
+                            actions: <Widget>[
+                              FlatButton(
+                                onPressed: () {
+                                  // User presses NO
+                                  Navigator.pop(context);
+                                },
+                                child: Text('No'),
+                              ),
+                              FlatButton(
+                                onPressed: () {
+                                  // User presses YES
+                                  taskData.deleteTask(task);
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Yes'),
+                              ),
+                            ],
+                          );
+                        },
+                      )
+                    : showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            title: Text('DELETE TASK'),
+                            content: Text('Do you wish to delete this task?'),
+                            actions: <Widget>[
+                              FlatButton(
+                                onPressed: () {
+                                  // User presses NO
+                                  Navigator.pop(context);
+                                },
+                                child: Text('No'),
+                              ),
+                              FlatButton(
+                                onPressed: () {
+                                  // User presses YES
+                                  taskData.deleteTask(task);
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Yes'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+              },
             );
           },
         );
